9521bb6ebc4f459b41acdb90e68d4a9b22e993ee
Romina Schann <cuqi213@gmail.com>
2021-03-30 10:04:05 +0900

    day_two challenge

---
+++ day_two.py
@@ -0,0 +1,43 @@
+"""
+As you can see, the code is broken.
+Create the missing functions, use default arguments.
+Sometimes you have to use 'return' and sometimes you dont.
+Start by creating the functions
+"""
+
+def is_on_list(li, day):
+  if(f"{day}" in li):
+    return True
+  else:
+    return False
+
+def get_x(li, index):
+  return f"{li[index]}"
+
+def add_x(li, day):
+  li.append(day)
+
+def remove_x(li, day):
+  li.remove(day)
+
+
+# \/\/\/\/\/\/\  DO NOT TOUCH AREA  \/\/\/\/\/\/\ #
+
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+print("Is Wed on 'days' list?", is_on_list(days, "Wed"))
+
+print("The fourth item in 'days' is:", get_x(days, 3))
+
+add_x(days, "Sat")
+print(days)
+
+remove_x(days, "Mon")
+print(days)
+
+
+# /\/\/\/\/\/\/\ END DO NOT TOUCH AREA /\/\/\/\/\/\/\ #
+
+
+
+
